f1308df2f9e0c2c2114d8d7d5086bbe32cb808b7
AnumShabbir <anumsh89@hotmail.com>
2018-06-13 15:05:36 -0400

    Homework04 final files

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,6 @@
 $(document).ready(function () {
     var wins = 0;
     var loses = 0;
-
     var targetnumber;
     var userscore = 0;
     var redgem;
@@ -9,65 +8,64 @@ $(document).ready(function () {
     var purplegem;
     var bluegem;
 
-    targetnumber = Math.floor(Math.random() * 50) + 10;
-    $('#targetnumber').text(targetnumber); //document.getElementById("targetnumber").innerText = targetnumber;
-    $('#userscore').text(userscore); //document.getElementById("userscore").innerText = userscore;
-    redgem = Math.floor(Math.random() * 10);
-    purplegem = Math.floor(Math.random() * 10);
-    greengem = Math.floor(Math.random() * 10);
-    bluegem = Math.floor(Math.random() * 10);
-    $("#win_msg").hide(); //document.getElementById("win_msg").style.cssText = "display: none";
-    $("#lose_msg").hide(); //document.getElementById("lose_msg").style.cssText = "display: none";
+//built-in Javascript function for target number between 19 and 120
+    function getTargetNum(min, max) {
+        return Math.floor(Math.random() * (max - min) ) + min;
+    }
 
+    targetnumber = getTargetNum(19,120);
+    $('#targetnumber').text(targetnumber); 
+    $('#userscore').text(userscore); 
+    redgem = Math.floor(Math.random() * 12) + 1; //for number between 1 to 12
+    purplegem = Math.floor(Math.random() * 12) + 1;
+    greengem = Math.floor(Math.random() * 12) + 1;
+    bluegem = Math.floor(Math.random() * 12) + 1;
+    console.log(redgem, bluegem, greengem, purplegem);
+    $("#win_msg").hide(); 
+    $("#lose_msg").hide(); 
 
+//built-in Javascript function for target number between 19 and 120
+function getTargetNum(min, max) {
+    return Math.floor(Math.random() * (max - min) ) + min;
+}
 
+//To keep up the score instead of refreshing the game
     function resetGame() {
         userscore = 0;
-        targetnumber = Math.floor(Math.random() * 50 + 10);
-        redgem = Math.floor(Math.random() * 10 + 1);
-        purplegem = Math.floor(Math.random() * 10 + 1);
-        greengem = Math.floor(Math.random() * 10 + 1);
-        bluegem = Math.floor(Math.random() * 10 + 1);
-        $('#targetnumber').text(targetnumber); //document.getElementById("targetnumber").innerText = targetnumber;
-        $('#userscore').text(userscore); //document.getElementById("userscore").innerText = userscore;
+        targetnumber = getTargetNum(19,120);
+        redgem = Math.floor(Math.random() * 12) + 1;
+        purplegem = Math.floor(Math.random() * 12) + 1;
+        greengem = Math.floor(Math.random() * 12) + 1;
+        bluegem = Math.floor(Math.random() * 12) + 1;
+        console.log(redgem, bluegem, greengem, purplegem);
+        $('#targetnumber').text(targetnumber); 
+        $('#userscore').text(userscore); 
     };
-
+//function if user wins
     function yay() {
         wins++
-        $("#lose_msg").hide();    //document.getElementById("lose_msg").style.cssText = "display: none";
-        $("#win_msg").show();    // document.getElementById("win_msg").style.cssText = "display: block";
-        $('#wins').text(wins);   //document.getElementById("wins").innerText = wins;
+        $("#lose_msg").hide();    
+        $("#win_msg").show();   
+        $('#wins').text(wins); 
         console.log("wins are:" + wins);
         resetGame();
     };
-
+//function if user loses
     function nay() {
         loses++;
-        $("#win_msg").hide();     //document.getElementById("win_msg").style.cssText = "display: none";
-        $("#lose_msg").show();    // document.getElementById("lose_msg").style.cssText = "display: block";
-        $("#loses").text(loses);   //document.getElementById("loses").innerText = loses;
+        $("#win_msg").hide();  
+        $("#lose_msg").show(); 
+        $("#loses").text(loses); 
         console.log("loses are:" + loses);
         resetGame();
     };
 
+// Converting crystal images to buttons 
     // -----------------RED GEM -----------------------------------
-    /*document.getElementById("redgem").addEventListener("click", function () {
-        userscore = userscore + redgem;
-        console.log("New userscore= " + userscore);
-        document.getElementById("userscore").innerText = userscore;
-        if (targetnumber < userscore) {
-        nay();
-        }
-        else if (targetnumber === userscore) {
-        yay();
-        }
-    
-    });*/
-
     $('#redgem').on('click', function () {
         userscore = userscore + redgem;
         console.log("New userscore= " + userscore);
-        $("#userscore").text(userscore);         // document.getElementById("userscore").innerText = userscore;
+        $("#userscore").text(userscore);  
         if (targetnumber < userscore) {
             nay();
         }
@@ -77,18 +75,6 @@ $(document).ready(function () {
     })
 
     // -----------------PURPLE GEM -----------------------------------
-    /*document.getElementById("purplegem").addEventListener("click", function () {
-        userscore = userscore + purplegem;
-        console.log("New userscore= " + userscore);
-        document.getElementById("userscore").innerText = userscore;
-        if (targetnumber < userscore) {
-            nay();
-        }
-        else if (targetnumber === userscore) {
-            yay();
-        }
-    });*/
-
     $('#purplegem').on('click', function () {
         userscore = userscore + purplegem;
         console.log("New userscore= " + userscore);
@@ -102,17 +88,6 @@ $(document).ready(function () {
     })
 
     // -----------------GREEN GEM -----------------------------------
-    /*document.getElementById("greengem").addEventListener("click", function () {
-        userscore = userscore + greengem;
-        console.log("New userscore= " + userscore);
-        document.getElementById("userscore").innerText = userscore;
-        if (targetnumber < userscore) {
-            nay();
-        }
-        else if (targetnumber === userscore) {
-            yay();
-        }
-    });*/
     $('#greengem').on('click', function () {
         userscore = userscore + greengem;
         console.log("New userscore= " + userscore);
@@ -127,18 +102,6 @@ $(document).ready(function () {
 
 
     // -----------------BLUE GEM -----------------------------------
-    /*document.getElementById("bluegem").addEventListener("click", function () {
-        userscore = userscore + bluegem;
-        console.log("New userscore= " + userscore);
-        document.getElementById("userscore").innerText = userscore;
-        if (targetnumber < userscore) {
-            nay();
-        }
-        else if (targetnumber === userscore) {
-            yay();
-        }
-    
-    });*/
     $('#bluegem').on('click', function () {
         userscore = userscore + bluegem;
         console.log("New userscore= " + userscore);
@@ -150,6 +113,5 @@ $(document).ready(function () {
             yay();
         }
     })
-
 
 });
